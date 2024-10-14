82f1e95e21088595ff5151e1a12587502bcd2607
unknown <auza.luiser@gmail.com>
2024-10-13 22:38:42 -0400

    version 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },  
+  { path: 'acercade', component: AboutComponent }, 
+  { path: '**', component: InicioComponent }, 
+];
